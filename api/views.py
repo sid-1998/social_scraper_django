1d11890990608931cf08d61f75ab7f0873617a98
vigzmv <vigzmv@outlook.com>
2017-04-20 03:03:07 +0530

    Done

--- api/views.py
+++ api/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from models import *
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.db.models import IntegerField
+from django.db.models.functions import Cast
 
 from bs4 import BeautifulSoup
 import requests
@@ -75,12 +78,26 @@ def codechef(request):
         response = urllib2.urlopen(url)
         soup = BeautifulSoup(response,'html.parser')
 
-        name = soup.select_one('body > main > div > div > div > div > div > header > h2').text
-        rating = soup.find('div', {"class":'rating-number'}).text
-        globalRank = soup.find_all('ul', {"class":'inline-list'})[1].find_all('strong')[0].text
-        countryRank = soup.find_all('ul', {"class":'inline-list'})[1].find_all('strong')[1].text
-        country = soup.find('span', {'class':'user-country-name'}).text
-
+        try:
+            name = soup.select_one('body > main > div > div > div > div > div > header > h2').text
+        except:
+            name = 'Not Found'
+        try:
+            rating = soup.find('div', {"class":'rating-number'}).text
+        except:
+            rating = 'Not Found'
+        try:
+            globalRank = soup.find_all('ul', {"class":'inline-list'})[1].find_all('strong')[0].text
+        except:
+            globalRank = 'Not Found'
+        try:
+            countryRank = soup.find_all('ul', {"class":'inline-list'})[1].find_all('strong')[1].text
+        except:
+            countryRank = 'Not Found'
+        try:
+            country = soup.find('span', {'class':'user-country-name'}).text
+        except:
+            country = 'Not Found'
 
         code = Codechef(
             username=username,
@@ -103,11 +120,22 @@ def quora(request):
         url = 'https://www.quora.com/profile/' + username
         response = urllib2.urlopen(url)
         soup = BeautifulSoup(response, 'html.parser')
-        print url
-        answers = soup.find('div', {'class':'list_header'}).text.split()[0]
-        totalViews = soup.find('div', {'class':'AboutListItem AnswerViewsAboutListItem'}).find('span',{'class':'main_text'}).text.split()[0]
-        monthViews = soup.find('div', {'class':'AboutListItem AnswerViewsAboutListItem'}).find('span',{'class':'detail_text'}).text.split()[0]
-        name = soup.find('div', {'class':'ProfileNameAndSig'}).find('span', {'class':'user'}).text
+        try:
+            answers = soup.find('div', {'class':'list_header'}).text.split()[0]
+        except:
+            answers = 'Not Found'
+        try:
+            totalViews = soup.find('div', {'class':'AboutListItem AnswerViewsAboutListItem'}).find('span',{'class':'main_text'}).text.split()[0]
+        except:
+            totalViews = 'Not Found'
+        try:
+            monthViews = soup.find('div', {'class':'AboutListItem AnswerViewsAboutListItem'}).find('span',{'class':'detail_text'}).text.split()[0]
+        except:
+            monthViews = 'Not Found'
+        try:
+            name = soup.find('div', {'class':'ProfileNameAndSig'}).find('span', {'class':'user'}).text
+        except:
+            name = 'Not Found'
 
         user =  request.user
         quora = Quora(
@@ -127,8 +155,8 @@ def stats(request):
     if not request.user.is_superuser:
         return redirect('/')
     else:
-        coders = Codechef.objects.all().order_by('-rating');
-        quoras = Quora.objects.all().order_by('-answers');
+        coders = Codechef.objects.annotate(ratings=Cast('rating',IntegerField())).order_by('-ratings');
+        quoras = Quora.objects.annotate(answer=Cast('answers',IntegerField())).order_by('-answer');
         context = {
             'coders': coders,
             'quoras': quoras,
