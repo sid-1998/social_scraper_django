238b19c8e474b437b2e9aa19fda96b31aab09050
vigzmv <vigzmv@outlook.com>
2017-06-18 00:53:05 +0530

    Complete

--- api/models.py
+++ api/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 # class ModelUser(models.Model):
 #     user = models.OneToOneField(User)
 
+
 class Codechef(models.Model):
     user = models.OneToOneField(User, blank=True, null=True)
     username = models.CharField(max_length=30, default="", blank=True)
@@ -16,8 +17,8 @@ class Codechef(models.Model):
     globalRank = models.CharField(max_length=30, default="", blank=True)
     countryRank = models.CharField(max_length=30, default="", blank=True)
 
+
 class Quora(models.Model):
-    user = models.OneToOneField(User, blank=True, null=True)
     username = models.CharField(max_length=30, default="", blank=True)
     answers = models.CharField(max_length=30, default="", blank=True)
     monthViews = models.CharField(max_length=30, default="", blank=True)
--- api/views.py
+++ api/views.py
@@ -15,6 +15,8 @@ import requests
 import urllib2
 import operator
 
+
+
 def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -29,11 +31,13 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'signup.html', context={'form': form})
 
+
+
 @login_required(login_url='/signup')
 def index(request):
-    current_user = request.user
+        Coder = Codechef.objects.get(user=current_user)
     try:
-        Coder = Codechef.objects.get(user = current_user)
+        Coder = Codechef.objects.get(user=current_user)
         cUsername = Coder.username
         cName = Coder.name
         cRating = Coder.rating
@@ -42,9 +46,9 @@ def index(request):
         cPlace = Coder.country
 
     except:
-        cUsername = cUame = cRating = cGlobal = cCountry = cPlace = cName = None
+        quora = Quora.objects.get(user=current_user)
     try:
-        quora = Quora.objects.get(user = current_user)
+        quora = Quora.objects.get(user=current_user)
         qUsername = quora.username
         qAnswers = quora.answers
         qTCount = quora.totalViews
@@ -53,67 +57,78 @@ def index(request):
 
     except:
         qUsername = qAnswers = qTCount = qMCount = qName = None
-
-    context = {
-        'user' : current_user,
-        'cUsername' : cUsername,
-        'cName' : cName,
-        'cRating' : cRating,
-        'cGlobal' : cGlobal,
-        'cCountry' : cCountry,
-        'cPlace' : cPlace,
-        'qUsername' : qUsername,
-        'qAnswers' : qAnswers,
-        'qTCount' : qTCount,
-        'qMCount' : qMCount,
-        'qName' : qName,
+        'user': current_user,
+        'cUsername': cUsername,
+        'cName': cName,
+        'cRating': cRating,
+        'cGlobal': cGlobal,
+        'cCountry': cCountry,
+        'cPlace': cPlace,
+        'qUsername': qUsername,
+        'qAnswers': qAnswers,
+        'qTCount': qTCount,
+        'qMCount': qMCount,
+        'qName': qName,
+        'qMCount': qMCount,
+        'qName': qName,
     }
+
 
     return render(request, 'home.html', context=context)
 
-def codechef(request):
+        username = request.POST.get('username')
+        user = request.user
     if request.method == 'POST':
-        username =  request.POST.get('username')
-        user =  request.user
-
-        url = 'https://www.codechef.com/users/' + username
-        response = urllib2.urlopen(url)
-        soup = BeautifulSoup(response,'html.parser')
+        username = request.POST.get('username')
+        user = request.user
+        soup = BeautifulSoup(response, 'html.parser')
 
         try:
-            name = soup.select_one('body > main > div > div > div > div > div > header > h2').text
+            name = soup.select_one(
+                'body > main > div > div > div > div > div > header > h2').text
+
+        try:
+            name = soup.select_one(
+            rating = soup.find('div', {"class": 'rating-number'}).text
         except:
             name = 'Not Found'
         try:
-            rating = soup.find('div', {"class":'rating-number'}).text
+            globalRank = soup.find_all(
+                'ul', {"class": 'inline-list'})[1].find_all('strong')[0].text
         except:
             rating = 'Not Found'
         try:
-            globalRank = soup.find_all('ul', {"class":'inline-list'})[1].find_all('strong')[0].text
+            countryRank = soup.find_all(
+                'ul', {"class": 'inline-list'})[1].find_all('strong')[1].text
+                'ul', {"class": 'inline-list'})[1].find_all('strong')[0].text
         except:
             globalRank = 'Not Found'
-        try:
-            countryRank = soup.find_all('ul', {"class":'inline-list'})[1].find_all('strong')[1].text
+            country = soup.find('span', {'class': 'user-country-name'}).text
+            countryRank = soup.find_all(
+                'ul', {"class": 'inline-list'})[1].find_all('strong')[1].text
         except:
             countryRank = 'Not Found'
         try:
-            country = soup.find('span', {'class':'user-country-name'}).text
+            country = soup.find('span', {'class': 'user-country-name'}).text
         except:
             country = 'Not Found'
 
         code = Codechef(
-            username=username,
-            user=user,
+            country=country,
+        )
             name=name,
             rating=rating,
             globalRank=globalRank,
             countryRank=countryRank,
-            country = country,
-            )
+            country=country,
+
+        )
 
         code.save()
 
     return redirect('/')
+
+
 
 def delCodechef(request):
     if request.method == 'POST':
@@ -121,64 +136,82 @@ def delCodechef(request):
 
     return redirect('/')
 
-def delQuora(request):
+
+
+        username = request.POST.get('username')
     if request.method == 'POST':
         Quora.objects.filter(user=request.user).delete()
 
     return redirect('/')
 
+            answers = soup.find(
+                'div', {'class': 'list_header'}).text.split()[0]
 def quora(request):
     if request.method == 'POST':
-        username =  request.POST.get('username')
+        username = request.POST.get('username')
 
         url = 'https://www.quora.com/profile/' + username
-        response = urllib2.urlopen(url)
+            totalViews = soup.find('div', {'class': 'AboutListItem AnswerViewsAboutListItem'}).find(
+                'span', {'class': 'main_text'}).text.split()[0]
         soup = BeautifulSoup(response, 'html.parser')
         try:
-            answers = soup.find('div', {'class':'list_header'}).text.split()[0]
+            answers = soup.find(
+            monthViews = soup.find('div', {'class': 'AboutListItem AnswerViewsAboutListItem'}).find(
+                'span', {'class': 'detail_text'}).text.split()[0]
             if answers == 'Answers':
                 answers = 'Not Found'
         except:
-            answers = 'Not Found'
+            name = soup.find('div', {'class': 'ProfileNameAndSig'}).find(
+                'span', {'class': 'user'}).text
         try:
-            totalViews = soup.find('div', {'class':'AboutListItem AnswerViewsAboutListItem'}).find('span',{'class':'main_text'}).text.split()[0]
-        except:
+            totalViews = soup.find('div', {'class': 'AboutListItem AnswerViewsAboutListItem'}).find(
+                'span', {'class': 'main_text'}).text.split()[0]
+        user = request.user
             totalViews = 'Not Found'
         try:
-            monthViews = soup.find('div', {'class':'AboutListItem AnswerViewsAboutListItem'}).find('span',{'class':'detail_text'}).text.split()[0]
+            monthViews = soup.find('div', {'class': 'AboutListItem AnswerViewsAboutListItem'}).find(
+                'span', {'class': 'detail_text'}).text.split()[0]
         except:
             monthViews = 'Not Found'
         try:
-            name = soup.find('div', {'class':'ProfileNameAndSig'}).find('span', {'class':'user'}).text
+            name = soup.find('div', {'class': 'ProfileNameAndSig'}).find(
+                'span', {'class': 'user'}).text
         except:
             name = 'Not Found'
 
-        user =  request.user
+
+        user = request.user
         quora = Quora(
             username=username,
             user=user,
             name=name,
-            totalViews=totalViews,
-            monthViews=monthViews,
+        coders = Codechef.objects.annotate(ratings=Cast(
+            'rating', IntegerField())).order_by('-ratings')
+        quoras = Quora.objects.annotate(answer=Cast(
+            'answers', IntegerField())).order_by('-answer')
             answers=answers,
         )
         quora.save()
 
     return redirect('/')
 
+
 @login_required(login_url='/signup')
 def stats(request):
     if not request.user.is_superuser:
         return redirect('/')
     else:
-        coders = Codechef.objects.annotate(ratings=Cast('rating',IntegerField())).order_by('-ratings')
-        quoras = Quora.objects.annotate(answer=Cast('answers',IntegerField())).order_by('-answer')
-        users = User.objects.all()
+        coders = Codechef.objects.annotate(ratings=Cast(
+            'rating', IntegerField())).order_by('-ratings')
+        quoras = Quora.objects.annotate(answer=Cast(
+            'answers', IntegerField())).order_by('-answer')
+            codechefScore = (codechefScore - minCodechef) / \
+                (maxCodechef - minCodechef)
 
         maxCodechef = coders.aggregate(Max('ratings'))['ratings__max']
         maxCodechef = float(maxCodechef)
         minCodechef = coders.aggregate(Min('ratings'))['ratings__min']
-        minCodechef = float(minCodechef)
+            quoraScore = (quoraScore - minQuora) / (maxQuora - minQuora)
 
         maxQuora = quoras.aggregate(Max('answer'))['answer__max']
         maxQuora = float(maxQuora)
@@ -186,15 +219,16 @@ def stats(request):
         minQuora = float(minQuora)
 
         # calculating score for users
-        for user in users:
+        users.sort(key=operator.attrgetter('totalScore'), reverse=True)
             codechefScore = list(coders.filter(user=user))[0].rating
             codechefScore = float(codechefScore)
-            codechefScore = (codechefScore - minCodechef)/(maxCodechef - minCodechef)
+            codechefScore = (codechefScore - minCodechef) / \
+                (maxCodechef - minCodechef)
             user.codechefScore = codechefScore
 
             quoraScore = list(quoras.filter(user=user))[0].answers
             quoraScore = float(quoraScore)
-            quoraScore = (quoraScore - minQuora)/(maxQuora - minQuora)
+            quoraScore = (quoraScore - minQuora) / (maxQuora - minQuora)
             user.quoraScore = quoraScore
 
             user.totalScore = (codechefScore * 1.0 + quoraScore * 1.0) / 2.0
@@ -202,7 +236,7 @@ def stats(request):
         users = list(users)
 
         # sorting list
-        users.sort(key = operator.attrgetter('totalScore'), reverse=True)
+        users.sort(key=operator.attrgetter('totalScore'), reverse=True)
 
         context = {
             'coders': coders,
